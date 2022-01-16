0a1f5df3bba728c8eaa2c181276283b97dc78e09
balanaQTF <84318100+balanaQTF@users.noreply.github.com>
2022-01-16 23:50:45 +0100

    get_no_days_falling method added

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
-import json
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import investpy
+import pandas as pd
 
 app = FastAPI()
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 
 df = investpy.get_stock_historical_data(stock='AAPL',
                                         country='United States',
                                         from_date='01/01/2020',
-                                        to_date='05/01/2020')
-print(df.head())
+                                        to_date='10/01/2020')
 
-result = df.to_json(orient="split")
-parsed = json.loads(result)
-json.dumps(parsed, indent=2)
+js = df.to_json(orient = 'table')
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {get_no_days_falling("jpm")}
 
 @app.get("/dupa")
 async def getItems():
-    return {tuple(parsed)}
+    return Response(js)
+
+@app.get("/dupa2")
+async def getItems():
+    return Response(js)
+
+
+def get_no_days_falling(stockname: str):
+    recent_data = get_recent_data_by_name(stockname)
+    print(recent_data)
+    number = 0
+    for tuple in recent_data[::-1].itertuples(index=False):
+        if tuple._5 > 0:
+            break  # break here
+        number += 1
+    return number
+
+
+def get_recent_data_by_name(stockname: str):
+    search_result = investpy.search_quotes(text=stockname, products=['stocks'],
+                                           countries=['united states'], n_results=1)
+    recent_data = search_result.retrieve_recent_data()
+    return recent_data
+
+    print()
+
+
+
+
+
+
+
--- test2.py
+++ test2.py
@@ -2,15 +2,16 @@ import json
 import investpy
 import time
 
-# Don't filter columns first
+# Filter dataframe to only one column
 
-search_result = investpy.search_quotes(text='JPM', products=['stocks'],
+search_result = investpy.search_quotes(text='apple', products=['stocks'],
                                        countries=['united states'], n_results=1)
 recent_data = search_result.retrieve_recent_data()
+filtered=recent_data[['Change Pct']]
 
-for tuple in recent_data.itertuples(index=False):
-    print(tuple._5)
+for tuple in filtered.itertuples(index=False):
+    print(tuple._0)
 
 start_time = time.time()
-print(recent_data)
+
 print("--- %s seconds ---" % (time.time() - start_time))
